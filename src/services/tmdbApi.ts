53893cce1abd0335dfc334c53affd72296539bc2
mickadelghafoor <208494767+mickadelghafoor@users.noreply.github.com>
2025-07-13 22:48:39 +0000

    Update tmdbApi.ts

--- src/services/tmdbApi.ts
+++ src/services/tmdbApi.ts
@@ -1,4 +1,4 @@
-const API_KEY = '6b01915a87f6ed072cae833644073a83';
+const API_KEY = env.process;
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
